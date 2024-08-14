df168aa5975281e8aa9aef708c9c20ccade2e7f6
Samzam-Cyber <svogel24@gmail.com>
2024-08-14 12:19:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector('h1').addEventListener('click', function() {
+    alert('Titre cliqué!');
+});
+function rechercherJoueur() {
+    alert('Fonctionnalité "Rechercher un joueur" à implémenter.');
+}
+
+function ajouterJoueur() {
+    alert('Fonctionnalité "Ajouter un joueur" à implémenter.');
+}
+
+function simulateur() {
+    alert('Fonctionnalité "Simulateur" à implémenter.');
+}
+
+function creerTournoi() {
+    alert('Fonctionnalité "Créer un tournoi" à implémenter.');
+}
+
+function gererTournoi() {
+    alert('Fonctionnalité "Gérer le tournoi" à implémenter.');
+}
